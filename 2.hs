728aad5e5feb1811e6c63a9ce89f6bd8a011bdec
freundf <fabio@freund.io>
2024-12-02 10:35:44 +0100

    add day 2, part 2

--- 2.hs
+++ 2.hs
@@ -19,10 +19,6 @@ maxDiff3 [x] = True
 maxDiff3 (x:y:xs)
     | abs (x - y) > 3 || x == y     = False
     | otherwise                     = maxDiff3 (y:xs)
---    where 
---        maxDiffAcc _ False      = False
---        maxDiffAcc [x] acc      = acc
---        maxDiffAcc (x:y:xs) acc = maxDiffAcc (y:xs) (abs (x - y) <= 3 && acc
 
 isSafe :: [Int] -> Bool
 isSafe x = (isIncr x || isDecr x) && maxDiff3 x
@@ -30,11 +26,18 @@ isSafe x = (isIncr x || isDecr x) && maxDiff3 x
 part1 :: [String] -> Int
 part1 i = length $ filter isSafe (map toInts i) 
 
-part2 i = 0
+
+removeEach :: [Int] -> [[Int]]
+removeEach l = [take i l ++ drop (i + 1) l | i <- [0 .. length l - 1]]
+
+anySafe :: [[Int]] -> Bool
+anySafe l = any (== True) (map isSafe l)
+
+part2 :: [String] -> Int
+part2 i = length $ filter anySafe (map (removeEach . toInts) i)
 
 
 main :: IO ()
 main = do
     input <- readLines 2
-    print $ filter isSafe (map toInts input)
     printSolution (part1 input) (part2 input)
