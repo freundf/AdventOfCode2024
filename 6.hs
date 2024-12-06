4adc9ebf81bfa733e3892513f14a555a61bbbb93
freundf <fabio@freund.io>
2024-12-06 17:12:36 +0100

    add day 6, part 2

--- 6.hs
+++ 6.hs
@@ -1,10 +1,16 @@
 module Main where
 
 import Util
-import Data.List (elem, elemIndex, transpose)
+import Data.List (elem, elemIndex)
 import Data.Maybe (fromJust)
 
-data Direction = CW | CCW
+type Position = (Int, Int)
+type Direction = (Int, Int)
+type Grid = [[Char]]
+data Fail
+
+directions :: [Direction]
+directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
 guard :: Char
 guard = '^'
@@ -15,46 +21,83 @@ obstacle = '#'
 visited :: Char
 visited = 'X'
 
-rotate :: Direction -> [[a]] -> [[a]]
-rotate CW   = map reverse . transpose
-rotate CCW  = reverse . transpose
-
-move :: [[Char]] -> Int
-move grid
-    | elem guard (head grid)    = countVisited grid
-    | otherwise                 = move $ nextMove grid
-
-nextMove :: [[Char]] -> [[Char]]
-nextMove grid = nextMoveHelper grid [] grid
+simulate :: Grid -> Grid
+simulate g = move initPos initDir g
     where
-        nextMoveHelper (r1:r2:rs) acc copy 
-            | elem guard r2     = if collision (r1, r2) then rotate CCW copy
-                                    else acc ++ moveGuard (r1, r2) ++ rs
-            | otherwise         = nextMoveHelper (r2:rs) (acc ++ [r1]) copy
-        
-collision :: (String, String) -> Bool
-collision (x, y) = x !! idx == obstacle
-    where idx = fromJust $ elemIndex guard y
+        move :: Position -> Direction -> Grid -> Grid
+        move p@(x, y) d@(dx, dy) g
+            | nextX < 0 || nextY < 0 || nextX >= sizeX || nextY >= sizeY    = g
+            | get (nextX, nextY) g == obstacle                              = move p (nextDirection d) g
+            | otherwise                                                     = move (nextX, nextY) d nextGrid
+            where
+                nextX = x + dx
+                nextY = y + dy
+                nextGrid = set (nextX, nextY) guard $ set p visited g
+                nextDirection x = directions !! (((fromJust $ elemIndex x directions) + 1) `mod` (length directions)) 
 
-moveGuard :: (String, String) -> [[Char]]
-moveGuard (x, y) = [replace idx guard x, replace idx visited y]
-    where idx = fromJust $ elemIndex guard y
-
-countVisited :: [[Char]] -> Int
-countVisited = length . filter isVisited . foldr (++) []
-    where 
-        isVisited x = x == guard || x == visited
-
-replace :: Int -> a -> [a] -> [a]
-replace i v xs = take i xs ++ [v] ++ drop (i + 1) xs
+        initDir = directions !! 0
+        initPos = indexOf '^' g
+        sizeX = length (head g)
+        sizeY = length g
 
 
-part1 = move
+simulateMax :: Int -> Grid -> Either Grid Bool
+simulateMax max g = move initPos initDir max 0 g
+    where
+        move :: Position -> Direction -> Int -> Int -> Grid -> Either Grid Bool
+        move p@(x, y) d@(dx, dy) max steps g
+            | steps > max                                                   = Right False
+            | nextX < 0 || nextY < 0 || nextX >= sizeX || nextY >= sizeY    = Left g
+            | get (nextX, nextY) g == obstacle                               = move p (nextDirection d) max steps g
+            | otherwise                                                     = move (nextX, nextY) d max (steps + 1) nextGrid
+            where
+                nextX = x + dx
+                nextY = y + dy
+                nextGrid = set (nextX, nextY) guard $ set p visited g
+                nextDirection x = directions !! (((fromJust $ elemIndex x directions) + 1) `mod` (length directions)) 
 
-part2 i = 0
+        initDir = directions !! 0
+        initPos = indexOf '^' g
+        sizeX = length (head g)
+        sizeY = length g
 
 
+
+indexOf :: Char -> Grid -> Position
+indexOf c g = indexCounter c g 0
+    where
+        indexCounter _ [] _ = error ("Couldn't find: " ++ [c])
+        indexCounter c (r:rs) counter
+            | c `elem` r    = (fromJust $ elemIndex c r, counter)
+            | otherwise     = indexCounter c rs (counter + 1)
+
+get :: Position -> Grid -> Char
+get (x, y) g = g !! y !! x
+
+set :: Position -> Char -> Grid -> Grid
+set (x, y) t g = take y g ++ [take x row ++ [t] ++ drop (x + 1) row] ++ drop (y + 1) g
+    where row = g !! y
+
+countVisited :: Grid -> Int
+countVisited = length . filter (`elem` [guard, visited]) . concat
+
+part1 :: Grid -> Int
+part1 = countVisited . simulate
+
+
+part2 i size = length $ filter fails $ variations2D $ resetGrid (indexOf '^' i) $ simulate i
+    where fails x = either (\x -> False) not $ simulateMax size x 
+
+resetGrid :: Position -> Grid -> Grid
+resetGrid initPos grid = set initPos '^' $ set (indexOf '^' grid) 'X' grid
+
+    
+variations2D :: [[Char]] -> [[[Char]]]
+variations2D grid = [set (i, j) '#' grid | i <- [0..length grid - 1], j <- [0..length (grid !! i) - 1], get (i, j) grid == visited]
+
+
+main :: IO ()
 main = do
     input <- readLines 6
-    printSolution (part1 input) (part2 input)
-
+    let size = (length input) * (length $ head input)
+    printSolution (part1 input) (part2 input size)
