aa715cadcd2d4c9b564f754746861c8bad3e8067
freundf <fabio@freund.io>
2024-12-09 21:49:21 +0100

    add day 9, part 2

--- 9.hs
+++ 9.hs
@@ -2,99 +2,88 @@ module Main where
 
 import Util
 import Data.Char (digitToInt)
+import Data.Ord (comparing)
+import Data.List (sortBy, delete, insertBy)
+import Data.Maybe (fromJust)
 
 
 -- File functions
-type ID = Int
-type Size = Int
-type File = (ID, Size)
 
-free :: ID
-free = -1
+type FileSystem = ([File], [FreeBlock])
+type FreeBlock = (Int, Int) -- (start, size)
+type File = (Int, Int, Int) -- (id, start, size)
 
-expandFile :: File -> [File]
-expandFile file@(id, size)
-    | size == 1     = [file]
-    | otherwise     = take size (repeat (id, 1))
+part1 i = 0
+part2 = checksum . compressNoFragmentation . parseFileSystem
 
-expandFS :: [File] -> [File]
-expandFS = concat . map expandFile
+-- Parse
+parseFileSystem :: [Char] -> FileSystem
+parseFileSystem xs = parseAcc xs ([], []) 0 0
+    where
+        parseAcc [] (files, frees) _ _ = (reverse (sortBy (comparing getId) files), sortBy (comparing fst) frees)
+        parseAcc [x] (files, frees) idx len = parseAcc [] (parseFile idx len (digitToInt x):files, frees) idx len
+        parseAcc (x:y:xs) (files, frees) idx len = parseAcc xs (parseFile idx len x' : files, parseFree (len + x') y' : frees) (idx + 1) (len + y' + x')
+            where
+                x' = digitToInt x
+                y' = digitToInt y
 
+parseFile :: Int -> Int -> Int -> File
+parseFile = (,,)
+
+parseFree :: Int -> Int -> FreeBlock
+parseFree = (,)
+
+
+-- FileSystem
+
+firstFree :: Int -> Int -> [FreeBlock] -> Maybe FreeBlock
+firstFree size before []       = Nothing
+firstFree size before (x:xs)
+    | size <= (snd x) && before > (fst x)   = Just x
+    | otherwise                             = firstFree size before xs
+
+compressNoFragmentation :: FileSystem -> FileSystem
+compressNoFragmentation fs = compressAcc fs []
+    where
+        compressAcc ([], frees) acc     = (acc, frees)
+        compressAcc ((f:fs), frees) acc = case free of
+            Nothing -> compressAcc (fs, frees) (f:acc)
+            Just x  -> compressAcc (fs, newFrees) (newFile:acc)
+            where
+                x = fromJust free
+                free = firstFree (getSize f) (getStart f) frees 
+                newFrees
+                    | snd x == (getSize f)  = mergeFree $ insertSort (getStart f, getSize f) $ delete x frees
+                    | otherwise             = mergeFree $ insertSort (getStart f, getSize f) $ insertSort (fst x + (getSize f), snd x - (getSize f)) $ delete x frees
+                    where
+                        insertSort = insertBy (comparing fst)
+                
+                newFile = (getId f, fst x, getSize f)
+
+mergeFree :: [FreeBlock] -> [FreeBlock]
+mergeFree [x]                   = [x]
+mergeFree (x:y:xs)
+    | fst y == fst x + snd x    = mergeFree ((fst x, snd x + snd y) : xs)
+    | otherwise                 = x : mergeFree (y:xs)
+
+sortFS :: FileSystem -> FileSystem
+sortFS (f, g) = (sortBy (comparing (\(x,y,z) -> y)) f, g)
+
+checksum :: FileSystem -> Int
+checksum = sum . map fileVal . fst
+    where
+        fileVal (id, start, size) = id * size * (2 * start + size - 1) `div` 2
+
+
+-- File
 getSize :: File -> Int
-getSize = snd
+getSize (id, start, size) = size
 
 getId :: File -> Int
-getId = fst
+getId (id, start, size) = id
 
-resize :: Int -> File -> File
-resize x (id, size) = (id, x)
-
-
-
-
--- Util
-
-interleave :: [a] -> [a] -> [a]
-interleave (x:xs) (y:ys)    = x:y:interleave xs ys
-interleave _ _              = []
-
-
-
--- FS operations
-
-compress :: ([File] -> [File]) -> [File] -> [File]
-compress strategy xs
-    | fst (firstFree xs) == -1  = xs
-    | otherwise                 = compress strategy $ strip $ strategy xs
-
-
-checksum :: [File] -> Int
-checksum = foldl buildSum 0 . zip [0..]
-    where
-        buildSum acc (faktor, (id, _))
-            | id == free    = acc
-            | otherwise     = acc + (faktor * id)
-
-fragment :: [File] -> [File]
-fragment xs
-    | (getSize freeFile) > (getSize back)     = take idx xs ++ [back] ++ [resize (getSize back) freeFile] ++ (init $ drop (idx + 1) xs)
-    | (getSize freeFile) == (getSize back)    = take idx xs ++ [back] ++ (init $ drop (idx + 1) xs)
-    | otherwise                         = take idx xs ++ [(getId back, getSize freeFile)] ++ (init $ drop (idx + 1) xs) ++ [resize (getSize freeFile) back]
-    where 
-        (idx, freeFile) = firstFree xs
-        freeSize = getSize freeFile
-        back = last xs
-
-strip :: [File] -> [File]
-strip xs
-    | getId (last xs) == free  = init xs
-    | otherwise             = xs
-
-firstFree :: [File] -> (Int, File)
-firstFree xs = firstFreeAcc xs 0
-    where
-        firstFreeAcc [] _ = (-1, (-1, -1))
-        firstFreeAcc (x:xs) acc
-            | fst x == free = (acc, x)
-            | otherwise     = firstFreeAcc xs (acc + 1)
-
-part1  = checksum . expandFS . compress fragment . parseInput
-
-part2 i = 0
-
-
-findFirstFitting :: Size -> [File] -> (Int, File)
-findFirstFitting size xs = fittingAcc size xs 0
-    where
-        fittingAcc _ [] _ = (-1, (-1, -1))
-        fittingAcc size (x:xs) acc
-            | getId x == free && getSize x >= size  = (acc, x)
-            | otherwise                             = fittingAcc size xs (acc + 1)
-
-
-
-parseInput :: [Char] -> [File]
-parseInput = zip (interleave [0..] $ repeat (-1)) . map digitToInt
+getStart :: File -> Int
+getStart (id, start, size) = start
 
 
 example = "2333133121414131402"
@@ -103,4 +92,4 @@ main :: IO ()
 main = do
     input <- readLines 9
     let line = head input
-    printSolution (part1 example) (part2 example)
+    printSolution (part1 example) (part2 line)
